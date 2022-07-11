afabe6a65c21acd2d6c3ca802d4e6faec333f8ca
pasindur2 <pasindur2pj@gmail.com>
2022-07-11 20:32:49 +0530

    Update scripts to reset the db by take a dump and restore it

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
 # Author : OpenMRS
-#Purpose : Take a dump from the database
+# Purpose : Take a dump from the database and restore the databse
 # Script follows here:
 
-mysqldump -u openmrs -p openmrs > openmrs.sql;
-echo "Success"
+DUMP="openmrs.sql"
+
+if [ -f "$DUMP" ]; then
+    echo "$DUMP exists."
+    mysql -u openmrs -p openmrs < openmrs.sql
+      echo "Database restore successful"
+else
+    echo "$DUMP does not exist."
+    mysqldump -u openmrs -p openmrs > openmrs.sql;
+      echo "Database dump successful"
+    mysql -u openmrs -p openmrs < openmrs.sql
+      echo "Database restore successful"
+fi
--- scripts/restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Author : OpenMRS
-#Purpose : Restore the clear version of database
-# Script follows here:
-
-mysql -u openmrs -p openmrs < openmrs.sql
-echo "Success"
