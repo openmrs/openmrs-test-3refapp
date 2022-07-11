29747fb862202197fa491cfe36f57cbda1cef666
jesse5555 <natukunda55455@gmail.com>
2022-07-11 05:39:39 +0300

    O3-1406: Fix patient search test
    This PR is an e2e test verifying that a user can search for a patient1

---
+++ cypress/integration/cucumber/step_definitions/refapp-3.x/06-patient-search/patient-search.js
@@ -0,0 +1,41 @@
+import {Given} from 'cypress-cucumber-preprocessor/steps';
+
+let patient_uuid = null;
+
+beforeEach("Take a dump of the database and restore",() => {
+    //Run the script using npm
+    cy.exec("npm run resetDB")
+})
+
+before({tags: '@patient-involved'}, () => {
+    cy.createPatient().then((user) => {
+        patient_uuid = user.uuid;
+    });
+})
+
+Given('the user login to the Outpatient Clinic', () => {    
+    cy.on('uncaught:exception', (err, runnable) => {
+    	console.log(err);
+    	return false;
+    });
+    cy.login();
+    cy.visit('home');
+})
+
+When('the user search for {string}', patientName => {
+    cy.get('button[name=SearchPatientIcon]').click();
+    cy.getByPlaceholder('Search for a patient by name or identifier number').type(patientName);
+    // Adding an artificial wait because typed texts takes some time to get updated
+    cy.wait(1000);
+    cy.getByPlaceholder('Search for a patient by name or identifier number').type('{enter}');
+})
+
+Then('the result should be {string}', result => {
+    cy.contains(result);
+    
+});
+
+after({tags: '@patient-involved'}, () => {
+    cy.deletePatient(patient_uuid);
+});
+
