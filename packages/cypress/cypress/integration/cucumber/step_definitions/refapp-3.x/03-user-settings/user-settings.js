6bb182970e05cd9d1228357feb2b712d52d5138b
pasindur2 <pasindur2pj@gmail.com>
2022-08-31 00:37:07 +0530

    Fix test scenario

--- packages/cypress/cypress/integration/cucumber/step_definitions/refapp-3.x/03-user-settings/user-settings.js
+++ packages/cypress/cypress/integration/cucumber/step_definitions/refapp-3.x/03-user-settings/user-settings.js
@@ -1,33 +1,18 @@
-import {Given, After, Before} from 'cypress-cucumber-preprocessor/steps';
-
-let patient_uuid = null;
-
-Before({tags: '@patient-involved'}, () => {
-    cy.createPatient().then((user) => {
-        patient_uuid = user.uuid;
-    });
-});
+import {Given, After} from 'cypress-cucumber-preprocessor/steps';
 
 Given('the user is logged in', () => {
     cy.login();
+    cy.visit('home');
 })
 
-Given('the user arrives on a patient’s chart', () => {
-    cy.visit(`patient/${patient_uuid}/chart`);
-});
-
-When('the user navigate back to home by clicking Home breadcrumb', () => {
-    cy.get('.breadcrumbs-container').contains('Home').click({force: true});
-});
-
-When('the user can change the locale to Spanish', () => {
+When('the user change the locale to Spanish', () => {
     cy.get('button[name="Users"]').click({force: true});
     cy.getByLabel('Select locale').select('es', {force: true});
 });
 
 Then('the text should change into spanish', () => {
-    cy.get('button[name="SearchPatientIcon"]').click({force: true});
-    cy.getByPlaceholder('Buscar un paciente');
+  cy.get('button[name=SearchPatientIcon]').click({force: true});
+  cy.getByPlaceholder('Buscar un paciente');
 });
 
 When('the user clicks on the users icon', () => {
@@ -45,8 +30,8 @@ When('the user change the location to {string}', (location) => {
     cy.contains('Confirm').click({force: true});
 });
 
-Then('the user should be on the patient’s chart', () => {
-    cy.url().should('include', `patient/${patient_uuid}/chart`);
+Then('the user should be on the home page', () => {
+    cy.url().should('include', `home`);
 });
 
 Then('the current location should be {string}', (location) => {
@@ -54,39 +39,27 @@ Then('the current location should be {string}', (location) => {
     cy.get('div[data-extension-id=location-changer]').contains(location);
 });
 
-When('the user clicks on logout', () => {
-    cy.contains('Logout').click({force: true});
-});
-
-Then('the user should be on the login page', () => {
-    cy.url().should('include', 'login');
-});
-
 const changeLocale = (locale) => {
-    const apiUrl = Cypress.env('API_BASE_URL');
-    const username = Cypress.env('ADMIN_USERNAME');
-    const uuid = Cypress.env('ADMIN_UUID');
-    const password = Cypress.env('ADMIN_PASSWORD');
-    const token = window.btoa(`${username}:${password}`);
-    cy.request({
-        method: 'POST',
-        url: `${apiUrl}/user/${uuid}`,
-        body: {
-            userProperties: {
-                defaultLocale: locale,
-            }
-        },
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ${token}`,
-        },
-    });
+  const apiUrl = Cypress.env('API_BASE_URL');
+  const username = Cypress.env('ADMIN_USERNAME');
+  const uuid = Cypress.env('ADMIN_UUID');
+  const password = Cypress.env('ADMIN_PASSWORD');
+  const token = window.btoa(`${username}:${password}`);
+  cy.request({
+    method: 'POST',
+    url: `${apiUrl}/user/${uuid}`,
+    body: {
+      userProperties: {
+        defaultLocale: locale,
+      }
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${token}`,
+    },
+  });
 };
 
 After({tags: '@locale'}, () => {
-    changeLocale('en');
-});
-
-After({tags: '@patient-involved'}, () => {
-    cy.deletePatient(patient_uuid);
+  changeLocale('en');
 });
