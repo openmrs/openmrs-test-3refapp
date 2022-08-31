ac8c1d09985945afa98fd1693dc5d5ab452a41b9
pasindur2 <pasindur2pj@gmail.com>
2022-08-31 22:10:06 +0530

    Minor changes in vitals test

--- packages/cypress/cypress/integration/cucumber/step_definitions/refapp-3.x/05-vitals-and-biometrics/vitals-and-biometrics.js
+++ packages/cypress/cypress/integration/cucumber/step_definitions/refapp-3.x/05-vitals-and-biometrics/vitals-and-biometrics.js
@@ -1,9 +1,9 @@
-import { Given, Before, When } from "cypress-cucumber-preprocessor/steps";
+import { Given } from "cypress-cucumber-preprocessor/steps";
 
 let identifier = null;
 let patient = null;
 
-Before({ tags: "@vitals-and-biometrics" }, () => {
+before({ tags: "@vitals-and-biometrics" }, () => {
   cy.generateIdentifier().then((generatedIdentifier) => {
     identifier = generatedIdentifier;
     cy.createPatient(identifier).then((generatedPatient) => {
