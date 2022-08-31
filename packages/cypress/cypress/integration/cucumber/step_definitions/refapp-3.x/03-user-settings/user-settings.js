c3742c2638f9c9f1bb44a6ece7ab0945a502cbd3
Pasindu Rupasinghe <77311602+pasindur99@users.noreply.github.com>
2022-08-31 10:12:41 +0530

    O3 1404: Fix user settings test (#6)
    * Create user settings 3.x E2E test
    
    * Create github work flow forthe test
    
    * Fix location in test
    
    * Remove error log in login
    
    * Fix conflicts and re run workflow
    
    * Fix test scenario
    
    * Minor fix

---
+++ packages/cypress/cypress/integration/cucumber/step_definitions/refapp-3.x/03-user-settings/user-settings.js
@@ -0,0 +1,72 @@
+import {Given, After} from 'cypress-cucumber-preprocessor/steps';
+
+Given('the user is logged in', () => {
+  cy.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+    cy.login();
+    cy.visit('home');
+})
+
+When('the user change the locale to Spanish', () => {
+    cy.get('button[name="Users"]').click({force: true});
+    cy.getByLabel('Select locale').select('es', {force: true});
+});
+
+And("user clicks on patient search option", () => {
+  cy.get('button[name="Users"]').click();
+  cy.get('button[name=SearchPatientIcon]').click();
+});
+
+Then('the text should change into spanish', () => {
+  cy.getByPlaceholder('Buscar un paciente');
+});
+
+When('the user clicks on the users icon', () => {
+    cy.get('button[name="Users"]').click();
+});
+
+Then('the current location should be there', () => {
+    const defaultLocation = Cypress.env('DEFAULT_LOCATION_NAME');
+    cy.get('div[data-extension-id=location-changer]').contains(defaultLocation);
+});
+
+When('the user change the location to {string}', (location) => {
+    cy.get('div[data-extension-id=location-changer]').contains('Change').click({force: true})
+    cy.contains(location).click({force: true});
+    cy.contains('Confirm').click({force: true});
+});
+
+Then('the user should be on the home page', () => {
+    cy.url().should('include', `home`);
+});
+
+Then('the current location should be {string}', (location) => {
+    cy.get('button[name="Users"]').click();
+    cy.get('div[data-extension-id=location-changer]').contains(location);
+});
+
+const changeLocale = (locale) => {
+  const apiUrl = Cypress.env('API_BASE_URL');
+  const username = Cypress.env('ADMIN_USERNAME');
+  const uuid = Cypress.env('ADMIN_UUID');
+  const password = Cypress.env('ADMIN_PASSWORD');
+  const token = window.btoa(`${username}:${password}`);
+  cy.request({
+    method: 'POST',
+    url: `${apiUrl}/user/${uuid}`,
+    body: {
+      userProperties: {
+        defaultLocale: locale,
+      }
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${token}`,
+    },
+  });
+};
+
+After({tags: '@locale'}, () => {
+  changeLocale('en');
+});
---
+++ packages/cypress/resources/features/refapp-3.x/03-user-settings/user-settings.feature
@@ -0,0 +1,19 @@
+Feature: User Settings
+
+  Background:
+    Given the user is logged in
+
+  @user-settings
+  @locale
+  Scenario: The user should be able to change the locale
+    When the user change the locale to Spanish
+    And user clicks on patient search option
+    Then the text should change into spanish
+
+  @user-settings
+  Scenario: The user should be able to change the location
+    When the user clicks on the users icon
+    Then the current location should be there
+    When the user change the location to "Inpatient Ward"
+    Then the user should be on the home page
+    And the current location should be "Inpatient Ward"
